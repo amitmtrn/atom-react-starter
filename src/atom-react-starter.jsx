8a6d0f7ed5b2fef3b6f7f8d42f42c48722c25261
amitmtrn <amit@mtrn.co.il>
2015-07-02 18:20:16 +0300

    adding atom commands

--- src/atom-react-starter.jsx
+++ src/atom-react-starter.jsx
@@ -1,16 +1,15 @@
 var React = require("react");
 
 module.exports = AtomReactStarter = {
-
+  panelItem: {},
   activate: function() {
-    var panelItem = {
-      item: document.createElement("div")
-    };
-    React.render(
-        <div>my src test</div>,
-        panelItem.item
-      );
-      atom.workspace.addBottomPanel(panelItem);
+      atom.commands.add('atom-workspace', {
+        'atom-react-starter:toggle': (function(_this) {
+          return function() {
+            return _this.toggle();
+          };
+        })(this)
+      });
     },
 
     deactivate: function() {
@@ -19,6 +18,16 @@ module.exports = AtomReactStarter = {
 
     serialize: function() {
 
+    },
+
+    toggle: function() {
+      AtomReactStarter.panelItem
+        .item = document.createElement("div");
+      React.render(
+        <div>my src test</div>,
+          AtomReactStarter.panelItem.item
+        );
+      atom.workspace.addBottomPanel(AtomReactStarter.panelItem);
     }
 
-};
+  };
